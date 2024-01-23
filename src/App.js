26af3fbfcc4026055842cb57756bbc97c6b88613
Gayath chandula <gayath.chandula@randomsoftware.net>
2024-01-23 15:36:31 +0530

    firebase init

--- src/App.js
+++ src/App.js
@@ -5,9 +5,23 @@ import Navbar from "../src/components/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Models from "./Pages/Models";
 import TestimonialsPage from "./Pages/TestimonialsPage";
- 
+import { initializeApp } from "firebase/app";
 
 function App() {
+
+            var firebaseConfig = {
+               apiKey: process.env.REACT_APP_API_KEY,
+               authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+               projectId: process.env.REACT_APP_PROJECT_ID,
+               storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+               messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+               appId: process.env.REACT_APP_APP_ID,
+               measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+        
+               initializeApp(firebaseConfig);
+          
   return (
     <>
       <Navbar />
