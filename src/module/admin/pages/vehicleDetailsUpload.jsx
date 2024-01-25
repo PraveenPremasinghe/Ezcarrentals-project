8495c6d9e044c61b9a83172591da31e06ace9ec6
Gayath chandula <gayath.chandula@randomsoftware.net>
2024-01-25 07:12:11 +0530

    view data and delete

--- src/module/admin/pages/vehicleDetailsUpload.jsx
+++ src/module/admin/pages/vehicleDetailsUpload.jsx
@@ -52,8 +52,6 @@ const VehicleForm = () => {
       useEffect(()=>{
         onAuthStateChanged(auth, (user) => {
             if (!user) {
-              // User is signed in, see docs for a list of available properties
-              // https://firebase.google.com/docs/reference/js/firebase.User
               const uid = user.uid;
               navigate("/admin-login")
  
